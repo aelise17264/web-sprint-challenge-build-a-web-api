7e5213a98aa045883efb5aee04fb51638a5f1f84
unknown <aelise17264@gmail.com>
2020-10-02 10:50:17 -0600

    completed crud operations for projects

--- routers/project-router.js
+++ routers/project-router.js
@@ -13,7 +13,29 @@ router.get('/', (req, res) => {
     })
 })
 
-router.post('/', (req, res) => {
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    projects.get(id)
+    .then(project => {
+        res.status(200).json(project)
+    })
+    .catch(error => {
+        res.status(500).json({message: "Error finding project"})
+    })
+})
+
+router.get('/:id/actions', (req, res) => {
+    const id = req.params.id;
+    projects.getProjectActions(id)
+    .then(action => {
+        res.status(200).json(action)
+    })
+    .catch(error => {
+        res.status(500).json({message: "Error finding actions"})
+    })
+})
+
+router.post('/', validateProject, (req, res) => {
     const thisProject = req.body
     projects.insert(thisProject)
     .then(item => {
@@ -25,15 +47,38 @@ router.post('/', (req, res) => {
     })
 })
 
-router.get('/:id', (req, res) => {
+router.put('/:id', validateProject, (req, res) => {
+    const newProject = req.body;
     const id = req.params.id;
-    projects.get(id)
+    projects.update(id, newProject)
+    .then(item => {
+        res.status(200).json(item)
+    })
+    .catch(error => {
+        res.status(500).json({message: 'Error updating project'})
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    projects.remove(id)
     .then(project => {
         res.status(200).json(project)
     })
     .catch(error => {
-        res.status(500).json({message: "Error finding project"})
+        res.status(500).json({message: 'Error removing project'})
     })
 })
+
+function validateProject(req, res, next){
+    const body = req.body;
+    const name = req.body.name;
+    const description = req.body.description;
+    if(!body || !name || !description){
+        res.status(400).json({message: 'Missing some crucial data'})
+    }else{
+        next()
+    }
+}
 
 module.exports = router
